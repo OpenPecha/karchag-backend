9f5c676d74ff04abfd9b16d4580c16bae26da5b5
Karma Tashi <karmatashi@Karmas-MacBook-Air.local>
2025-06-16 13:10:20 +0530

    added the readme

--- database.py
+++ database.py
@@ -8,7 +8,17 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./buddhist_library.db")
+
+# For SQLite, we need to add some additional configuration
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL, 
+        connect_args={"check_same_thread": False}  # Needed for SQLite
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
